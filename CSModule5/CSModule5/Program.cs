928d6eccf06fa05bf1ce52df6d059ea6ba573ecb
Esteban Salomon <esalomon6492@southwestuniversity.edu>
2021-05-28 16:04:34 -0600

    CS Module 5

---
+++ CSModule5/CSModule5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSModule5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            for (var i = 1; i <= 20; i++)
+            {
+                result = result + i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
+
+
+
+
+ 
